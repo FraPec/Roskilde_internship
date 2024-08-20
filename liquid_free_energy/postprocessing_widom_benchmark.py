210faa2de9473817479682469c0b7938dcddb77d
FraPec <frapecorella@hotmail.it>
2024-08-20 13:11:16 +0200

    Fixed plot for Widom benchmark analysis

--- liquid_free_energy/postprocessing_widom_benchmark.py
+++ liquid_free_energy/postprocessing_widom_benchmark.py
@@ -13,15 +13,18 @@ if __name__=='__main__':
     
     fig.suptitle(r'$times$ and $\sigma_{\mu_{ex}}$ VS number of $ghost$ particles, $\rho=0.1$, $T=2.0$', fontsize=25)
     ### Plot of times against number of ghost particles
-    ax1.scatter(num_ghost_particles, time_v, marker='x')
+    ax1.scatter(num_ghost_particles, time_v / num_ghost_particles, marker='x')
     ax1.grid()
-    ax1.set_ylabel(r'times [s]', fontsize=25)
+    ax1.set_xscale('log', base=10)
+    ax1.set_yscale('log', base=10)
+    ax1.set_ylabel(r'$\frac{time}{n_{g}} [s]$', fontsize=25)
 
     ### Plot of sigma against number of ghost particles
     ax2.scatter(num_ghost_particles, sigma_v / np.abs(mu_v), marker='x')
     ax2.set_xscale('log', base=10)
+    ax2.set_yscale('log', base=10)
     ax2.grid()
-    ax2.set_xlabel(r'num. ghost particles', fontsize=30)
+    ax2.set_xlabel(r'num. ghost particles $\equiv n_{g}$', fontsize=30)
     ax2.set_ylabel(r'$\sigma / \mu_{ex}$', fontsize=25)
  
     plt.show()
